f57e537644d35a02cf28b92102c689e1a83ed9e9
evannguyen <evannguyen85@gmail.com>
2019-03-13 22:58:10 +0700

    add contact-form component and style the contact section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { NavigationDirective } from './directives/navigation/navigation.directive';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CollapseDirective } from './directives/collapse/collapse.directive';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 
 
@@ -31,6 +32,7 @@ import { CollapseDirective } from './directives/collapse/collapse.directive';
     NavigationDirective,
     NavbarComponent,
     CollapseDirective,
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
