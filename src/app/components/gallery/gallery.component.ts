39a858fd0a0b91295f44b8af1b4e10efad3ca7a7
evannguyen <evannguyen85@gmail.com>
2019-04-15 17:04:55 +1000

    made dialog generic to be shared for different projects and added dialog for tester performance dashboard proj

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -15,11 +15,79 @@ export class GalleryComponent implements OnInit {
   ngOnInit() {
   }
 
-  openDialog() {
+  openDialogForDashboard() {
     const dialogRef = this.dialog.open(DialogComponent, {
       width: '85%',
-      // height: '80%',
-      data: 'This is passed into the dialog. Hardcoded for now! To be changed depending on project'
+      data: {
+        title: 'TESTER PERFORMANCE DASHBOARD',
+        subtitle: 'Real-time tester monitoring',
+        avatar: '../../../assets/images/dashboard-icon.png',
+        imgSource: '../../../assets/images/dashboard.png',
+        desc:  `Tester performance dashboard to show how tester performs.`,
+        techs: [
+          {
+            name: 'MongoDB',
+            logo: '../../../assets/images/logos/mongodb.png'
+          },
+          {
+            name: 'NodeJS',
+            logo: '../../../assets/images/logos/node.js.png'
+          },
+          {
+            name: 'Express',
+            logo: '../../../assets/images/logos/express.png'
+          },
+          {
+            name: 'Highcharts',
+            logo: '../../../assets/images/logos/highcharts.png'
+          }
+        ],
+        links: {
+          site: 'https://boiling-brook-33207.herokuapp.com/',
+          git: 'https://github.com/evannguyen85/MyCalculator'
+        }
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog Closed: ${result}`);
+      this.dialogResult = result;
+    });
+  }
+
+  openDialogForCalculator() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '85%',
+      data: {
+        title: 'CALCULATOR',
+        subtitle: 'Responsive calculator',
+        avatar: '../../../assets/images/calc-icon.png',
+        imgSource: '../../../assets/images/calculator.png',
+        desc:  `A calculator to peform basic and scientific calculations.
+                This is built using html, css, javascript and bootstrap.
+                The app runs on Heroku so it may take several seconds to start up initially.`,
+        techs: [
+          {
+            name: 'HTML',
+            logo: '../../../assets/images/logos/html.png'
+          },
+          {
+            name: 'CSS',
+            logo: '../../../assets/images/logos/css.png'
+          },
+          {
+            name: 'JavaScript',
+            logo: '../../../assets/images/logos/javascript.png'
+          },
+          {
+            name: 'BootStrap',
+            logo: '../../../assets/images/logos/bootstrap.png'
+          }
+        ],
+        links: {
+          site: 'https://boiling-brook-33207.herokuapp.com/',
+          git: 'https://github.com/evannguyen85/MyCalculator'
+        }
+      }
     });
 
     dialogRef.afterClosed().subscribe(result => {
